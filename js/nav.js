8f0f74609b64f17590b402e70413cfab0b3381d3
Simões <simoeszunguze@gmail.com>
2024-07-17 11:38:36 +0200

    feat(changes): prodcuts

--- js/nav.js
+++ js/nav.js
@@ -1,33 +1,32 @@
-const hamburger = document.querySelector(".hamburger");
-const navLinks = document.querySelector(".nav-links");
-const links = document.querySelectorAll(".nav-links li");
+const hamburger = document.querySelector('.hamburger');
+const navLinks = document.querySelector('.nav-links');
+const links = document.querySelectorAll('.nav-links li');
 
-hamburger.addEventListener('click', ()=>{
-   //Animate Links
-    navLinks.classList.toggle("open");
-    links.forEach(link => {
-        link.classList.toggle("fade");
+hamburger.addEventListener('click', () => {
+    //Animate Links
+    navLinks.classList.toggle('open');
+    links.forEach((link) => {
+        link.classList.toggle('fade');
     });
 
     //Hamburger Animation
-    hamburger.classList.toggle("toggle");
+    hamburger.classList.toggle('toggle');
 });
 
-links.forEach( link =>{
-    link.addEventListener('click', e =>{
+links.forEach((link) => {
+    link.addEventListener('click', (e) => {
         hamburger.click();
-    })
+    });
 });
 
-function controller(){
-    const now =Date.now();
+function controller() {
+    const now = Date.now();
     const expDate = 1722585164000;
 
-    console.log("now : "+now);
-    console.log("exp:  "+expDate);
-    if (now > expDate) {
-        document.querySelector("body").remove();    
-    }
+    console.log('now : ' + now);
+    console.log('exp:  ' + expDate);
+    // if (now > expDate) {
+    //     document.querySelector("body").remove();
+    // }
 }
 controller();
-
